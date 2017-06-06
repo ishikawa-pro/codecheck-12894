a1513dc097f2604cdf2b409e524eaf6ed0754c35
ishikawa-pro <ishikawa.pro@gmail.com>
2017-06-06 21:30:59 +0900

    [add]途中経過

--- app/main.rb
+++ app/main.rb
@@ -1,3 +1,38 @@
+def calc(int_1, int_2, op)
+    case op.chop
+    when "+"
+        return int_1.to_i + int_2.to_i
+    when "-"
+        return int_1.to_i - int_2.to_i
+    when "*"
+        return int_1.to_i * int_2.to_i
+    when "/"
+        return int_1.to_i / int_2.to_i
+
+    end
+end
+
+def Reverse_polish(formula)
+    result = String.new
+
+    if formula.length == 5
+        int_1 = formula[0,1]
+        int_2 = formula[4,4]
+        op = formula[2,2]
+
+        result << int_1  << " " << int_2 << " " << op << "= "
+        result << calc(int_1,int_2, op).to_s
+        return result
+    end
+end
+
+def shorFormula(formula)
+    puts formula
+    #正規表現でカッコの中を見つける
+    md = formula.match(/[*\(\d\s.\s\d\)*]/)
+    return md[0].to_s[1,5]
+end
+
 def main(argv)
   # このコードは引数と標準出力を用いたサンプルコードです。
   # このコードは好きなように編集・削除してもらって構いません。
@@ -5,8 +40,13 @@ def main(argv)
   # This is a sample code to use arguments and outputs.
   # Edit and remove this code as you like.
 
-  argv.each_index do |i|
-    v = argv[i]
-    puts "argv[#{i}]: #{v}"
-  end
+    result = String.new
+    v = argv[0]
+    short_fm = shorFormula(v)
+    result << Reverse_polish(v)
+    
+#    v.chars do |char|
+#        puts char.to_i
+#    end
+    puts result
 end
